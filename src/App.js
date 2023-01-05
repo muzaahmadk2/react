9c4340680a41fb91a3c3bcfcccedf2b5cda614e7
muzaahmadk2 <muzaahmadk2@gmail.com>
2023-01-05 21:10:43 +0400

    expense -item-using-pros

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
 import ExpenseItem from './components/ExpenseItem.js';
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      loc:'delhi',
+    },
+    { 
+      id: 'e2', 
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12),
+      loc:'delhi', 
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      loc:'delhi',
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      loc:'delhi',
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ExpenseItem></ExpenseItem>
+        
+<ExpenseItem title={expenses[0].title}amount={expenses[0].amount}date={expenses[0].date}loc={expenses[0].loc}></ExpenseItem>
+<ExpenseItem title={expenses[1].title}amount={expenses[1].amount}date={expenses[1].date}loc={expenses[1].loc}></ExpenseItem>
+<ExpenseItem title={expenses[2].title}amount={expenses[2].amount}date={expenses[2].date}loc={expenses[2].loc}></ExpenseItem>
+<ExpenseItem title={expenses[3].title}amount={expenses[3].amount}date={expenses[3].date}loc={expenses[3].loc}></ExpenseItem>
+        
+        
       </header>
     </div>
   );
--- src/components/ExpenseItem.js.js
+++ src/components/ExpenseItem.js.js
@@ -1,10 +1,16 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
   return (
-    <div>
-      <h2>Expense Item!</h2>
-      <h5>food Rs 10</h5>
-      <h5>petrol RS 100</h5>
-      <h5>movies Rs 200</h5>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__description">{props.loc}</div>
+        <div className="expense-item__price">${props.amount}</div>
+        
+      </div>
     </div>
   );
 }
