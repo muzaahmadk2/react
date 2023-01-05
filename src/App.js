1c1c500bee957c444e6b38c80cd0b39ae2a74dd4
muzaahmadk2 <muzaahmadk2@gmail.com>
2023-01-05 19:38:44 +0400

    Expense Item -basic -first component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ExpenseItem from './components/ExpenseItem.js';
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <ExpenseItem></ExpenseItem>
       </header>
     </div>
   );
---
+++ src/components/ExpenseItem.js.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Item!</h2>
+      <h5>food Rs 10</h5>
+      <h5>petrol RS 100</h5>
+      <h5>movies Rs 200</h5>
+    </div>
+  );
+}
+export default ExpenseItem;
