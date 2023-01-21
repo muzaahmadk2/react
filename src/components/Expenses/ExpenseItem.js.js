7f5c8e8cf5f00d9912da9cf43e3374f86fc65d31
muzaahmadk2 <muzaahmadk2@gmail.com>
2023-01-21 18:15:22 +0400

    button using useState to update expense amount

--- src/components/Expenses/ExpenseItem.js.js
+++ src/components/Expenses/ExpenseItem.js.js
@@ -1,22 +1,28 @@
+import React, { useState } from 'react' ;
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
+
 function ExpenseItem(props) {
 
+  const [amount, setAmount] = useState(props.amount);
+
   const clickHandler = () =>{
+    setAmount(100);
     console.log("clicked!!!!!!!!!");
-  }
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
         title={props.title}
-        amount={props.amount}
+        amount={amount}
         loc={props.loc}
       />
-      <button onClick={clickHandler}>delete</button>
+      <button onClick={clickHandler}>Change-Expense</button>
     </Card>
   );
 }
